72a5b3fe5bb45a71d99182ce0434409c60d4fc4b
Roberto Lucas <andler.dev@gmail.com>
2024-11-04 18:30:22 -0600

    impr: site cache control

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,29 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'api.dicebear.com'],
-  }
+  },
+  headers() {
+    return [
+      {
+        source: '/static/*', // cache static assets
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=3600' },
+        ],
+      },
+      {
+        source: '/api/*', // cache API responses
+        headers: [
+          { key: 'Cache-Control', value: 'max-age=3600, must-revalidate' },
+        ],
+      },
+      {
+        source: '/(.*)', // cache all other routes
+        headers: [
+          { key: 'Cache-Control', value: 'max-age=3600, must-revalidate' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
